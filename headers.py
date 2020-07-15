95947c7b72fe564da4d49570e0f45890cdaa8125
black-t-ech <64723792+black-t-ech@users.noreply.github.com>
2020-07-16 02:40:23 +0530

    Update headers.py

--- headers.py
+++ headers.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python
-# WebPwn3r is a Web Applications Security Scanner
-# By Ebrahim Hegazy - twitter.com/zigoo0
-# First demo conducted 12Apr-2014 @OWASP Chapter Egypt
-# https://www.owasp.org/index.php/Cairo
+#WebScan Is A Tool For Scanning Website For Vulnerablities 
+#By Black_t_ech :: Telegram @black_t_ech_bot
+
 import urllib
 import re
 import time
